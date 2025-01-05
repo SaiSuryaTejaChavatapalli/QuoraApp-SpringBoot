fa58be3089e5eceb9f2a2af34742e8c717be4a24
Sai Surya Teja <saisuryateja3@gmail.com>
2025-01-05 11:53:15 +0530

    Initial Project Configuration

---
+++ src/main/java/com/sst/Quora_SpringBoot/QuoraSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sst.Quora_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoraSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sst/Quora_SpringBoot/QuoraSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sst.Quora_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoraSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
